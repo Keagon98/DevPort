5005a66ca952e16dba895728d0858cb2bc25c74a
Keagon98 <keaggzb20v@gmail.com>
2021-09-06 00:10:12 +0200

    Added GSAP scroll animation

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,1 +1,17 @@
 gsap.from(".fade-in", {duration: 1, opacity: 0, y: 150, stagger: 0.25});
+
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.from(".on-scroll", {
+    scrollTrigger: {
+        trigger:"about-section",
+        markers: true,
+        toggleActions: "restart pause reverse pause"
+    }, // start the animation when ".box" enters the viewport (once)
+    opacity: 0, 
+    y: 150, 
+    stagger: 0.25,
+    duration: 1
+  });
+
+ 
