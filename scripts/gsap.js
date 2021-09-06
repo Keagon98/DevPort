e92f817538b6c58987273c8427b3692bbbba515d
Keagon98 <keaggzb20v@gmail.com>
2021-09-06 18:04:14 +0200

    WIP: Mobile Responsiveness

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,10 +1,15 @@
-gsap.from(".fade-in", {duration: 1, opacity: 0, y: 150, stagger: 0.25});
+gsap.from(".fade-in", {
+  duration: 1, 
+  opacity: 0, 
+  y: 150, 
+  stagger: 0.25
+});
 
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.from(".on-scroll", {
     scrollTrigger: {
-        trigger:"about-section",
+        trigger:"on-scroll",
         toggleActions: "restart pause reverse pause"
     }, // start the animation when ".box" enters the viewport (once)
     opacity: 0, 
@@ -12,5 +17,8 @@ gsap.from(".on-scroll", {
     stagger: 0.25,
     duration: 1
   });
+
+
+  
 
  
