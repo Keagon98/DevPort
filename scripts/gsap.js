320f8b9b8449f9dbf38c81c148b0c4b499b18703
Keagon98 <keaggzb20v@gmail.com>
2021-09-07 23:41:53 +0200

    WIP: Added scroll animation on project images

--- scripts/gsap.js
+++ scripts/gsap.js
@@ -1,24 +1,45 @@
 gsap.from(".fade-in", {
-  duration: 1, 
-  opacity: 0, 
-  y: 150, 
-  stagger: 0.25
+  duration: 1,
+  opacity: 0,
+  y: 150,
+  stagger: 0.25,
 });
 
-
 gsap.registerPlugin(ScrollTrigger);
+
 gsap.from(".on-scroll", {
-    scrollTrigger: {
-        trigger:"on-scroll",
-        toggleActions: "restart pause reverse pause"
-    }, // start the animation when ".box" enters the viewport (once)
-    opacity: 0, 
-    y: 150, 
-    stagger: 0.25,
-    duration: 1
-  });
+  scrollTrigger: {
+    trigger: "on-scroll",
+    toggleActions: "restart pause reverse pause",
+  }, // start the animation when ".box" enters the viewport (once)
+  opacity: 0,
+  y: 150,
+  stagger: 0.25,
+  duration: 1,
+});
 
+let masks = document.querySelectorAll('.mask');
 
-  
+masks.forEach( mask => {
+    let image = mask.querySelectorAll('.img-holder');
 
- 
+    let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: mask,
+            toggleActions: "restart none none reset"
+        }
+    });
+
+    tl.set(mask, {autoAlpha: 1});
+
+    tl.from(mask, 1.5, {
+        xPercent: -100,
+        ease: Power2.out
+    });
+    tl.from(image, 1.5, {
+        xPercent: 100,
+        scale: 2,
+        delay: -1.5,
+        ease: Power2.out
+    });
+});
